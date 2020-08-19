bbea1c387bfce991dcc419b1d977b22cab65abb4
Diego Magalhaes <dmlmagal@gmail.com>
2020-08-18 23:07:55 -0300

    Add subscription methods

--- tests/signature_tests.py
+++ tests/signature_tests.py
@@ -1,11 +1,13 @@
 import unittest
 from os import environ
-from datetime import datetime
 import requests
-from random import choices
-from pyrecard.signature import plans, customer
+from pyrecard.signature import plan, customer, subscription
 from exceptions import MissingKey
 from common.urls import get_url
+from tests.mocker import mock_plan, mock_customer, mock_subscription
+
+
+environ['WIRECARD_KEY'] = '01010101010101010101010101010101:ABABABABABABABABABABABABABABABABABABABAB'
 
 
 class PlanTestCase(unittest.TestCase):
@@ -15,18 +17,7 @@ class PlanTestCase(unittest.TestCase):
     '''
 
     def setUp(self):
-        environ['WIRECARD_KEY'] = '01010101010101010101010101010101:ABABABABABABABABABABABABABABABABABABABAB'
-        self.data = {
-            'code': ''.join(choices('ABCDEF1234567890', k=10)),
-            'name': 'Plano Especial',
-            'description': 'Descrição do Plano Especial',
-            'amount': 990,
-            'setup_fee': 500,
-            'max_qty': 1,
-            'interval': {'length': 1, 'unit': 'MONTH'},
-            'billing_cycles': 12,
-            'payment_method': 'CREDIT_CARD'
-        }
+        self.data = mock_plan()
 
     def test_wirecard_sandbox_must_be_accessible(self):
         response = requests.get(get_url())
@@ -36,44 +27,47 @@ class PlanTestCase(unittest.TestCase):
     def test_raise_error_if_key_is_missing(self):
         environ['WIRECARD_KEY'] = ''
         with self.assertRaises(MissingKey):
-            plans.create(self.data)
+            plan.create(self.data)
 
     def test_plan_with_valid_data_should_be_created(self):
-        response = plans.create(self.data)
+        response = plan.create(self.data)
         self.assertEqual(response.status_code, 201)
 
     def test_plan_with_invalid_data_should_not_be_created(self):
         self.data['code'] = ''
-        response = plans.create(self.data)
+        response = plan.create(self.data)
         self.assertEqual(response.status_code, 400)
         self.assertTrue('errors' in response.json())
 
     def test_plan_should_be_changed(self):
-        plans.create(self.data)
+        plan.create(self.data)
         old_amount = self.data['amount']
         self.data['amount'] = 1000
         code = self.data.pop('code')
-        plans.alter(code, self.data)
-        response = plans.fetch(code)
+        plan.alter(code, self.data)
+        response = plan.fetch(code)
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.json().get('amount'), old_amount)
 
     def test_all_plans_must_be_returned(self):
-        response = plans.fetch_all()
+        response = plan.fetch_all()
         self.assertEqual(response.status_code, 200)
         self.assertTrue('plans' in response.json())
         self.assertGreater(len(response.json()['plans']), 1)
 
     def test_plan_must_be_inactivated_and_reactivated(self):
-        plans.create(self.data)
-        response = plans.fetch(self.data['code'])
+        plan.create(self.data)
+        response = plan.fetch(self.data['code'])
         self.assertEqual(response.json()['status'], 'ACTIVE')
-        plans.inactivate(self.data['code'])
-        response = plans.fetch(self.data['code'])
+        plan.inactivate(self.data['code'])
+        response = plan.fetch(self.data['code'])
         self.assertNotEqual(response.json()['status'], 'ACTIVE')
-        plans.activate(self.data['code'])
-        response = plans.fetch(self.data['code'])
+        plan.activate(self.data['code'])
+        response = plan.fetch(self.data['code'])
         self.assertEqual(response.json()['status'], 'ACTIVE')
+
+    def tearDown(self):
+        environ['WIRECARD_KEY'] = '01010101010101010101010101010101:ABABABABABABABABABABABABABABABABABABABAB'
 
 
 class CustomersTestCase(unittest.TestCase):
@@ -83,36 +77,7 @@ class CustomersTestCase(unittest.TestCase):
     '''
 
     def setUp(self):
-        environ['WIRECARD_KEY'] = '01010101010101010101010101010101:ABABABABABABABABABABABABABABABABABABABAB'
-        self.data = {
-            'code': ''.join(choices('ABCDEF1234567890', k=10)),
-            'email': 'test@user.com',
-            'fullname': 'Test User',
-            'cpf': '22222222222',
-            'phone_area_code': '11',
-            'phone_number': '934343434',
-            'birthdate_day': '26',
-            'birthdate_month': '04',
-            'birthdate_year': '1980',
-            'address': {
-                'street': 'Street Name',
-                'number': '100',
-                'complement': '',
-                'district': 'Neighboor Name',
-                'city': 'City Name',
-                'state': 'MG',
-                'country': 'BRA',
-                'zipcode': '00000000',
-            },
-            'billing_info': {
-                'credit_card': {
-                    'holder_name': 'Test User',
-                    'number': '4111111111111111',
-                    'expiration_month': '06',
-                    'expiration_year': str(datetime.now().year+2)[2:]
-                }
-            }
-        }
+        self.data = mock_customer()
 
     def test_customer_with_valid_data_must_be_created(self):
         response = customer.create(self.data)
@@ -140,3 +105,60 @@ class CustomersTestCase(unittest.TestCase):
         response = customer.change_card(self.data['code'], credit_card)
         self.assertEqual(response.status_code, 200)
         self.assertTrue('message' in response.json())
+
+
+class SubscriptionTestCase(unittest.TestCase):
+    '''
+       The following data has been taken and adapted from
+       https://dev.wirecard.com.br/v1.5/reference#assinaturas
+    '''
+
+    def setUp(self):
+        self.data = mock_subscription()
+
+    def test_signature_must_be_created(self):
+        response = subscription.create(self.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['message'], 'Assinatura criada com sucesso')
+
+    def test_signature_must_be_created_with_new_customer(self):
+        self.data['customer'] = mock_customer()
+        response = subscription.create(self.data, True)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['message'], 'Assinatura criada com sucesso')
+
+    def test_subscription_details_must_be_returned(self):
+        subscription.create(self.data)
+        response = subscription.fetch(self.data['code'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['code'], self.data['code'])
+
+    def test_all_subscriptions_must_be_returned(self):
+        response = subscription.fetch_all()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('subscriptions' in response.json())
+        self.assertGreater(len(response.json()['subscriptions']), 1)
+
+    def test_subscription_status_must_changed(self):
+        subscription.create(self.data)
+        response = subscription.set_status(self.data['code'], 'suspend')
+        self.assertEqual(response.status_code, 200)
+        response = subscription.set_status(self.data['code'], 'activate')
+        self.assertEqual(response.status_code, 200)
+        response = subscription.set_status(self.data['code'], 'cancel')
+        self.assertEqual(response.status_code, 200)
+
+    def test_subscription_data_must_be_changed(self):
+        subscription.create(self.data)
+        data = mock_plan()
+        plan.create(data)
+        self.data['plan']['code'] = data['code']
+        code = self.data.pop('code')
+        del self.data['customer']
+        response = subscription.change(code, self.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_subscription_payment_method_must_be_changed(self):
+        subscription.create(self.data)
+        response = subscription.set_payment_method(self.data['code'], 'BOLETO')
+        self.assertEqual(response.status_code, 200)
